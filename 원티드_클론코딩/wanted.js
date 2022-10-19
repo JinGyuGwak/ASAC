409ae93229d360da275e2fdf5460a1d5e9d8b1ae
JinGyuGwak <wlsrb2695@naver.com>
2022-10-19 10:25:31 +0900

    메뉴바 mouseout 설정 적용

--- 원티드_클론코딩/wanted.js
+++ 원티드_클론코딩/wanted.js
@@ -1,6 +1,9 @@
 var a=document.getElementById("menuBar");
 var menu_ul=document.getElementById("menuHover"); // me=ul임
+var bodypage=document.getElementById("bodypage");
+
 a.addEventListener("mouseover",menuShow);
+bodypage.addEventListener("mouseover",menudown);
 
 var lightbox = document.getElementById("lightbox");  // 라이트 박스. querySelector("#lightbox")도 가능
 var lightbox_info =document.getElementById("lightbox_info");
@@ -18,8 +21,14 @@ var email_submit_btn=document.getElementById("email_submit_btn");
 email_submit_btn.addEventListener("click",goInfo);
 set_btn.addEventListener("click", showLightbox);
 
-function menuShow(){   
+function menudown(){
+    //햄버거 메뉴에서 벗어나면서 menu_ul 이 아웃 될 경우 display none으로 하고 싶은데 
+    //함수를 하나 더 만들어야 하나?
+    menu_ul.style.display="none";
 
+}
+
+function menuShow(){
     if(menu_ul.style.display =="none"){
         menu_ul.style.display="block";
     }
@@ -27,8 +36,6 @@ function menuShow(){
         menu_ul.style.display="none";
     }
 }
-
-
 
 modalX.onclick = function() {  //click 이벤트가 발생했을 때 실행할 함수 선언
     email_text.value="";
