379ab5a9f2ae1885563467067233593b80b7d666
JinGyuGwak <wlsrb2695@naver.com>
2022-10-25 14:43:56 +0900

    no message

--- 3주차_react/hello-react/src/App.js
+++ 3주차_react/hello-react/src/App.js
@@ -1,35 +1,21 @@
-import { Component } from "react";
-import IterationSample from "./IterationSample";
-import LifeCycleSample from "./LifeCycleSample";
+import { useState } from "react";
+import Info from "./Info";
+import Average from "./Average";
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
-      </div>
-    );
-  }
-}
-
-export default App;
-
-/*
-함수형 컴포넌트
-const App =() =>{
-  return <ValidationSample/>;
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Average />}
+    </div>
+  );
 };
-*/
+export default App;
